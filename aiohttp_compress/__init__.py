a0123359e2c20f7e5fa76fec888c2088b5f2ef14
Dmitry Orlov <me@mosquito.su>
2021-02-16 15:51:31 +0300

    import version

--- aiohttp_compress/__init__.py
+++ aiohttp_compress/__init__.py
@@ -1,6 +1,6 @@
 from .middleware import compress_middleware
-
+from .version import __author__, __version__, version_info
 
 __all__ = (
-    "compress_middleware",
+    "compress_middleware", "version_info", "__author__", "__version__"
 )
